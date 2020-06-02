8eb429e54c6ae4d01da952511b3848c7ba9acf9f
Gabriel <gabriel@Gabriel-Mac-mini.local>
2020-06-02 18:37:34 -0300

    Criando o backend da aplicação

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,14 @@
 import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+import path from 'path';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    console.log('userasdasds');
-    response.json([
-        'gabriel'
-    ]);
-});
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 app.listen(3000);
